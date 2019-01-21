81b4d588a34b8fbac5957a2c74820e4e927e3289
Lavesh Panjwani <lpanjwani@outlook.com>
2019-01-21 23:16:55 +0400

    Q1 Almost Done

--- Test_2/Practice/src/practice/practice.java
+++ Test_2/Practice/src/practice/practice.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javaapplication52;
+package practice;
 
 import java.math;
 
@@ -11,7 +11,7 @@ import java.math;
  *
  * @author The
  */
-public class JavaApplication52 {
+public class Practice {
 
     /**
      * @param args the command line arguments
